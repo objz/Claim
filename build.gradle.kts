55ae9f10fcc99bc452a0a0d3c4aeda050d833826
objz <me@objz.dev>
2025-12-17 21:40:47 +0100

    changed commandpapi dependency to not shade it

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     java
-    id("com.gradleup.shadow") version "9.3.0"
 }
 
 group = "dev.objz"
@@ -17,22 +16,9 @@ repositories {
 }
 
 dependencies {
-    
     compileOnly("dev.folia:folia-api:1.21.8-R0.1-SNAPSHOT")
     compileOnly("net.dmulloy2:ProtocolLib:5.4.0")
     compileOnly("de.bluecolored:bluemap-api:2.7.7")
-    
-    implementation("dev.jorel:commandapi-paper-shade:11.1.0")
+    compileOnly("dev.jorel:commandapi-paper-core:11.1.0")
 }
 
-tasks {
-    shadowJar {
-        archiveClassifier.set("")
-        relocate("dev.jorel.commandapi", "dev.objz.libs.commandapi")
-    }
-    
-    assemble {
-        dependsOn(shadowJar)
-    }
-    
-}
